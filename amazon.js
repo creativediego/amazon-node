9dc40af4d57338b5b051a34fc58a310e36b3c030
Developer <developer@Creatives-MacBook-Pro.local>
2018-04-19 14:28:49 -0700

    version 1

--- amazon.js
+++ amazon.js
@@ -28,7 +28,7 @@ connection.connect(function(err) {
 function displayProducts() {
 
     connection.query("SELECT * FROM products", function(err, res) {
-        console.log("\n" + "Welcome! These are products available to purchase:" + "\n")
+        console.log("\n" + "Welcome! These are products available to purchase:" + "\n");
 
         console.table(res);
         promptPurchase();
@@ -47,9 +47,9 @@ function promptPurchase() {
             message: "Enter the ID of the product you would like to buy.",
             validate: function(value) {
                 if (isNaN(value) === false) {
-                    return true
+                    return true;
                 } else {
-                    return false
+                    return false;
                 }
             }
 
@@ -61,65 +61,59 @@ function promptPurchase() {
             message: "How many would you like to purchase?",
             validate: function(value) {
                 if (isNaN(value) === false) {
-                    return true
+                    return true;
                 } else {
-                    return false
+                    return false;
                 }
             }
         }
         //inquirer callback
     ]).then(answers => {
 
-
-        //Check if quantity can be met
-        //Read the database where id matches the user input id
-        connection.query("SELECT * FROM products WHERE ?", { id: answers.id }, function(err, res) {
-
-            //If the product ID exists
-            if (!res[0] === false) {
-
-                //Check stock
-                if (answers.quantity > res[0].quantity) {
-                    console.log(`Sorry, the quantity you requested exceeds the current stock of ${res[0].quantity}. Try placing the order again.`)
-                    promptPurchase();
-                } else {
-                    //execute outside function that updates the db
-                    updatedQuantity = res[0].quantity - parseInt(answers.quantity);
-
-                    processOrder();
-
-                    function processOrder() {
-                        connection.query("UPDATE products SET ? WHERE ?", [
-                            { quantity: updatedQuantity },
-                            { id: answers.id }
-
-                        ], function(err) {
-                            if (err) throw err;
-                            console.log(`Order of ${res[0].product_name} has been placed succesfully! Total: $${res[0].price}`)
-                            displayProducts();
-
-                        })
-
-
-                    }
-
-                }
-
-                //If product ID doesn't exist
-            } else {
-
-                console.log("The product ID you entered does not exist.")
-                promptPurchase();
-            }
-
-        });
+        processOrder(answers);
 
     });
 
+}
+
+function processOrder(answers) {
+
+    //Check if quantity can be met
+    //Read the database where id matches the user input id
+    connection.query("SELECT * FROM products WHERE ?", { id: answers.id }, function(err, res) {
+
+        //If the product ID exists
+        if (!res[0] === false) {
+
+            //Check stock
+            if (answers.quantity > res[0].quantity) {
+                console.log(`Sorry, the quantity you requested exceeds the current stock of ${res[0].quantity}. Try placing the order again.`);
+                promptPurchase();
+            } else {
+                //execute outside function that updates the db
+                updatedQuantity = res[0].quantity - parseInt(answers.quantity);
 
 
+                connection.query("UPDATE products SET ? WHERE ?", [
+                    { quantity: updatedQuantity },
+                    { id: answers.id }
+
+                ], function(err) {
+                    if (err) throw err;
+                    console.log(`Order of ${res[0].product_name} has been placed succesfully! Total: $${res[0].price}`);
+                    connection.end();
 
 
+                });
 
+            }
 
+            //If product ID doesn't exist
+        } else {
+
+            console.log("The product ID you entered does not exist. Try again.");
+            promptPurchase();
+        }
+
+    });
 }
