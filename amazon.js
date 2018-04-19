eed3db8ad6106f2355e57d3ad4ab76f5b903d264
Developer <developer@Creatives-MacBook-Pro.local>
2018-04-19 03:05:41 -0700

    added display products and prompt purchase

--- amazon.js
+++ amazon.js
@@ -1,40 +1,120 @@
 //dependencies
 
-let sql = require("mysql");
+let mysql = require("mysql");
 
 let inquirer = require("inquirer");
 
 //establish mysql connection
-let db = mysql.createConnection({
+let connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
+    user: "root",
     password: "",
-    database: bamazon
+    database: "bamazon"
 
 });
 
 //function to connect to mysql
-db.connect(function(err) {
-    if (err) throw error;
-    console.log(`connected as id ${connection.threadID}`);
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    displayProducts();
+
 });
+
 //function that displays a table of current products
 function displayProducts() {
 
-    db.query("SELECT * FROM products", function(err, res) {
-
-
+    connection.query("SELECT * FROM products", function(err, res) {
+        console.log("Products available to purchase:")
+        console.log("-------------------------------------")
+        for (let i = 0; i < res.length; i++) {
+            console.log(`Item: ${res[i].product_name} | Product ID: ${res[i].id} | Quantity: ${res[i].quantity} Price: ${res[i].price} | Department: ${res[i].department}`);
+        }
+        console.log("-------------------------------------")
+            //connection.end();
+        promptPurchase();
     });
 
 }
 //inquirer function that prompts the user to purchase a product
-//ask for product id
-//ask for quantity
+function promptPurchase() {
 
-//inquirer callback
-//if quantiy in answers is equal or less than the input, proceed with the...
-//update function that decreses the product quantity 
-//and displays a successful checkout message, including total purchase cost
-//else display a message that the quantity is insufficient
-//recursively prompt the user again for product
-//bonus: if quantity is 0, prompt the user for a new product; else, just prompt for quantity
+
+    inquirer.prompt([
+        //ask for product id
+        {
+            type: "input",
+            name: "id",
+            message: "Enter the ID of the product you would like to buy.",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true
+                } else {
+                    return false
+                }
+            }
+
+        },
+        //ask for quantity
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many would you like to purchase?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true
+                } else {
+                    return false
+                }
+            }
+        }
+        //inquirer callback
+    ]).then(answers => {
+
+
+        //Check if quantity can be met
+        //Read the database where id matches the user input id
+        connection.query("SELECT quantity FROM products WHERE ?", { id: answers.id }, function(err, res) {
+
+            //If the product ID exists
+            if (!res[0] === false) {
+
+                //Check stock
+                if (answers.quantity > res[0].quantity) {
+                    console.log(`Sorry, the quantity you requested exceeds the current stock of ${res[0].quantity}. Try placing the order again`)
+                    promptPurchase();
+                } else {
+                    //execute outside function that updates the db
+                    function processOrder() {
+
+
+
+                    }
+                    console.log("Order placed!")
+                    promptPurchase();
+                }
+
+                //If product ID doesn't exist
+            } else {
+
+                console.log("The product ID you entered does not exist.")
+                promptPurchase();
+            }
+
+        });
+
+    });
+
+
+
+
+    //if quantiy in answers is equal or less than the input, proceed with the...
+    //update function that decreses the product quantity 
+    //and displays a successful checkout message, including total purchase cost
+    //else display a message that the quantity is insufficient
+    //recursively prompt the user again for product
+    //bonus: if quantity is 0, prompt the user for a new product; else, just prompt for quantity
+
+
+}
--- db-deploy.sql
+++ db-deploy.sql
@@ -15,31 +15,31 @@ PRIMARY KEY (id)
 );
 
 INSERT INTO products (product_name, price, department, quantity)
-VALUES ("iPhone 8", 799, "electronics", 20)
+VALUES ("iPhone 8", 799, "electronics", 20);
 
 INSERT INTO products (product_name, price, department, quantity)
-VALUES ("MacBook Pro i7 16GB RAM", 1500, "electronics", 80)
+VALUES ("MacBook Pro i7 16GB RAM", 1500, "electronics", 80);
 
 INSERT INTO products (product_name, price, department, quantity)
-VALUES ("MacBook Pro i5 4GB RAM with Fancy Apple Sticker", 2800, "electronics", 2)
+VALUES ("MacBook Pro i5 4GB RAM with Fancy Apple Sticker", 2800, "electronics", 2);
 
 INSERT INTO products (product_name, price, department, quantity)
-VALUES ("Playstation 4 same-thing-but-more-expensive edition", 499, "electronics", 10)
+VALUES ("Playstation 4 same-thing-but-more-expensive edition", 499, "electronics", 10);
 
 INSERT INTO products (product_name, price, department, quantity)
-VALUES ("Teaching assitant cheatsheet: How to BS your way through any TA job", 19.99, "books", 10)
+VALUES ("TA cheatsheet: How to BS your way through any TA job", 19.99, "books", 10);
 
 INSERT INTO products (product_name, price, department, quantity)
-VALUES ("Holy shit, I'm a developer: How to use your new programming superpowers to make the world a better place", 9.99, "books", 10)
+VALUES ("How to Nodejs for Dummies", 9.99, "books", 10);
 
 INSERT INTO products (product_name, price, department, quantity)
-VALUES ("Life-size motivational Steve Jobs cardboard figure", 999.98, "office supplies", 50)
+VALUES ("Life-size motivational Steve Jobs cardboard figure", 999.98, "office supplies", 50);
 
 INSERT INTO products (product_name, price, department, quantity)
-VALUES ("Luke Skywalker lightsaber", 15, "toys", 17)
+VALUES ("Luke Skywalker lightsaber", 15, "toys", 17);
 
 INSERT INTO proucts (product_name, price, department, quantity)
-VALUES ("XXL Trojans 69 Pack", 69.69, "lifesavers", 69)
+VALUES ("XXL Trojans 69 Pack", 69.69, "lifesavers", 69);
 
 INSERT INTO products (product_name, price, department, quantity)
-VALUES ("TRUMP Steaks", 0.01, "disposables", 9999)
+VALUES ("TRUMP Steaks", 0.01, "disposables", 9999);
