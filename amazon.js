5a0db9e7d383d729e7532634387344875ddd0eb5
Developer <developer@Creatives-MacBook-Pro.local>
2018-04-19 14:04:38 -0700

    added processOrder function and mysql table module

--- amazon.js
+++ amazon.js
@@ -4,6 +4,8 @@ let mysql = require("mysql");
 
 let inquirer = require("inquirer");
 
+const cTable = require('console.table');
+
 //establish mysql connection
 let connection = mysql.createConnection({
     host: "localhost",
@@ -26,13 +28,9 @@ connection.connect(function(err) {
 function displayProducts() {
 
     connection.query("SELECT * FROM products", function(err, res) {
-        console.log("Products available to purchase:")
-        console.log("-------------------------------------")
-        for (let i = 0; i < res.length; i++) {
-            console.log(`Item: ${res[i].product_name} | Product ID: ${res[i].id} | Quantity: ${res[i].quantity} Price: ${res[i].price} | Department: ${res[i].department}`);
-        }
-        console.log("-------------------------------------")
-            //connection.end();
+        console.log("\n" + "Welcome! These are products available to purchase:" + "\n")
+
+        console.table(res);
         promptPurchase();
     });
 
@@ -75,24 +73,36 @@ function promptPurchase() {
 
         //Check if quantity can be met
         //Read the database where id matches the user input id
-        connection.query("SELECT quantity FROM products WHERE ?", { id: answers.id }, function(err, res) {
+        connection.query("SELECT * FROM products WHERE ?", { id: answers.id }, function(err, res) {
 
             //If the product ID exists
             if (!res[0] === false) {
 
                 //Check stock
                 if (answers.quantity > res[0].quantity) {
-                    console.log(`Sorry, the quantity you requested exceeds the current stock of ${res[0].quantity}. Try placing the order again`)
+                    console.log(`Sorry, the quantity you requested exceeds the current stock of ${res[0].quantity}. Try placing the order again.`)
                     promptPurchase();
                 } else {
                     //execute outside function that updates the db
-                    function processOrder() {
+                    updatedQuantity = res[0].quantity - parseInt(answers.quantity);
 
+                    processOrder();
+
+                    function processOrder() {
+                        connection.query("UPDATE products SET ? WHERE ?", [
+                            { quantity: updatedQuantity },
+                            { id: answers.id }
+
+                        ], function(err) {
+                            if (err) throw err;
+                            console.log(`Order of ${res[0].product_name} has been placed succesfully! Total: $${res[0].price}`)
+                            displayProducts();
+
+                        })
 
 
                     }
-                    console.log("Order placed!")
-                    promptPurchase();
+
                 }
 
                 //If product ID doesn't exist
@@ -109,12 +119,7 @@ function promptPurchase() {
 
 
 
-    //if quantiy in answers is equal or less than the input, proceed with the...
-    //update function that decreses the product quantity 
-    //and displays a successful checkout message, including total purchase cost
-    //else display a message that the quantity is insufficient
-    //recursively prompt the user again for product
-    //bonus: if quantity is 0, prompt the user for a new product; else, just prompt for quantity
+
 
 
 }
